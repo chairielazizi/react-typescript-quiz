ef7b8078589f4ff61205ec27f12e900084d5dc1d
chairielazizi <azizihr2000@gmail.com>
2020-09-04 23:08:07 +0800

    create functional component in question card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const QuestionCard = () => <div>Question Card</div>;
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: any;
+  questionNum: number;
+  totalQuestion: number;
+};
+
+// create functional component using React.FC
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answer,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestion,
+}) => (
+<div>
+    <p className="number">
+        Question: {questionNum} / {totalQuestion}
+    </p>
+    <p dangerouslySetInnerHTML={{__html: question }} />
+    <div>
+        {answer.map(answer => (
+            <div>
+                <button disabled={userAnswer} onClick={callback}>
+                    <span dangerouslySetInnerHTML={{ __html: answer}} />
+                </button>
+            </div>
+        ))}
+    </div>
+</div>);
 
 export default QuestionCard;
+)
