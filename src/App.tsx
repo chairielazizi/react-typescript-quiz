8976d339816c21f63e64b851bb7854c6c275f88d
chairielazizi <azizihr2000@gmail.com>
2020-09-04 23:15:56 +0800

    adding props to QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 
 // components
-import QuestionCard from './components/QuestionCard';
+import QuestionCard from "./components/QuestionCard";
+
+const TOTALQ = 10;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
-  const startTrivia = async() => {
+  const startTrivia = async () => {};
 
-  }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
-const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
-} 
-
-const nextQuestion = () => {
-
-}
+  const nextQuestion = () => {};
 
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
       <button className="start" onClick={startTrivia}>
-        Start 
+        Start
       </button>
       <p className="score">Score: </p>
       <p>Loading Questions ...</p>
-      <QuestionCard />
-      <button className="next" onClick={nextQuestion} >
+      <QuestionCard
+        questionNum={number + 1}
+        totalQuestion={TOTALQ}
+        question={questions[number].question}
+        answer={questions[number].answer}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
-
     </div>
   );
-}
+};
 
 export default App;
